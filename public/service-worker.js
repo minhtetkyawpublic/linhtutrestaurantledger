8e13d5dc20e71e9cc1e1fa915072d00ba774714f
MinHtetKyaw <minhtetkyaw.public@gmail.com>
2026-08-21 11:59:15 +0630

    Use page scrolling for customer list

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@ const APP_SCOPE_PATH = new URL(self.registration.scope).pathname.replace(
     "",
 );
 const API_PREFIX = `${APP_SCOPE_PATH}/api`;
-const STATIC_CACHE = "linh-tut-restaurant-shell-42e667d84adc";
+const STATIC_CACHE = "linh-tut-restaurant-shell-f3bed8c7b70d";
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
